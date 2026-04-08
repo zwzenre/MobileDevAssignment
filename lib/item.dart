ff3bd7b72da7c707373b9fce9b13a119c3f908bc
Mingyverse <teoh.yongming456@gmail.com>
2026-04-08 18:25:59 +0800

    Initial stful placeholders

---
+++ lib/item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Item extends StatefulWidget {
+  const Item({super.key});
+
+  @override
+  State<Item> createState() => _ItemState();
+}
+
+class _ItemState extends State<Item> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
